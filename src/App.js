f9ddabdec9d20c9af99f874b0c141b89ff4ed351
Jay <julesmontojo1@gmail.com>
2022-06-18 07:10:41 +0900

    feat: GI-9 add-search-filter-functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { Component } from 'react';
-// import {db} from './firestore/config';
-// import {collection, getDocs } from 'firebase/firestore'
 import Fish from './entities/Fish'
 import './App.css';
 
@@ -10,7 +8,8 @@ class App extends Component {
     
     this.state = {
       isFetching: false,
-      fishDataDB: []
+      fishDataDB: [],
+      searchValue: '',
     }
   }
   componentDidMount(){
@@ -19,17 +18,25 @@ class App extends Component {
     
   async fetchAllFishHandler() {
     this.setState(
-      () => {return { isFetching: true}},
+      () => {return { isFetching: true}}
     );
 
     const fishData = await Fish.getAll()
     this.setState(
       () => {return { fishDataDB: fishData}},
-      () => {console.log(this.state.fishDataDB)}
     );
     
     this.setState(
-      () => {return { isFetching: false}},
+      () => {return { isFetching: false}}
+    );
+  }
+
+  filterDisplayedFish(value) {
+    const results = this.state.fishDataDB.filter((fish) => {
+      return fish.name_en.toLowerCase().includes(value);
+    })
+    this.setState(
+      () => {return { fishDataDB: results}}
     );
   }
 
@@ -39,9 +46,9 @@ class App extends Component {
         <input 
           className='search-box'
           type='search'
-          placeholder='search monsters'
+          placeholder='search fish'
           onChange={(e) => {
-            console.log("...typing")
+            this.filterDisplayedFish(e.target.value.toLowerCase())
           }}
         ></input>
         {
