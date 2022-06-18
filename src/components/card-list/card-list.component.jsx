182b0fda7386855f91c3ee0f19725d8a29842a8e
Jay <julesmontojo1@gmail.com>
2022-06-19 04:05:13 +0900

    feat: 15-move-card-list-logic-to-component

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,9 +2,19 @@ import { Component } from 'react';
 
 class CardList extends Component {
   render() {
+    const { className, listData } = this.props;
+ 
     return (
-      <div>
-        <h1>Card List Test</h1>
+      <div className= {`card-list ${className}`}>
+        {
+          listData.map((fish) => {
+            return (
+              <div key={fish.id}>
+                <h1>{fish.name_jp}({fish.name_en})</h1>
+              </div>
+            )
+          })
+        }
       </div>
     )
   }
